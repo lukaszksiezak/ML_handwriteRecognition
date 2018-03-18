01d9eb5c94908078520f15b754f0cb40a402cb43
Lukasz Ksiezak <lukasz.ksiezak@pl.abb.com>
2018-03-18 21:49:51 +0100

    Modified program to train NN with real data

--- main.py
+++ main.py
@@ -1,6 +1,73 @@
 import scipy.io
 import numpy as np
 from matplotlib import pyplot as plt
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
+
+def learn_nn_network(X_train, X_test, y_train, y_test, batches):
+
+    learning_rate = 0.5
+    epochs = 10
+
+    # input x - for 20 x 20 pixels = 400
+    x = tf.placeholder(tf.float32, [None, 400])
+    #  output data placeholder - 10 digits
+    y = tf.placeholder(tf.float32, [None, 1])
+
+    # weights connecting the input to the hidden layer (100 nodes in hidden
+    # layer)
+    W1 = tf.Variable(tf.random_normal([400, 100], stddev=0.03), name='W1')
+    b1 = tf.Variable(tf.random_normal([100]), name='b1')
+
+    # weights connecting the hidden layer to the output layer
+    W2 = tf.Variable(tf.random_normal([100, 10], stddev=0.03), name='W2')
+    b2 = tf.Variable(tf.random_normal([10]), name='b2')
+
+    hidden_out = tf.add(tf.matmul(x, W1), b1)
+    hidden_out = tf.nn.relu(hidden_out)
+
+    # hidden layer output - in this case, let's use a softmax
+    # activated output layer
+    y_ = tf.nn.softmax(tf.add(tf.matmul(hidden_out, W2), b2))
+
+    # operation converting the output y_ to a clipped version,
+    # limited between 1e-10 to 0.999999. (Avoiding log(0))
+    y_clipped = tf.clip_by_value(y_, 1e-10, 0.9999999)
+    cross_entropy = -tf.reduce_mean(
+        tf.reduce_sum(
+            y * tf.log(
+                y_clipped) + (1 - y) * tf.log(
+                    1 - y_clipped), axis=1))
+
+    optimiser = tf.train.GradientDescentOptimizer(
+        learning_rate=learning_rate).minimize(cross_entropy)
+
+    # setup the initialisation operator
+    init_op = tf.global_variables_initializer()
+
+    # define an accuracy assessment operation
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    with tf.Session() as sess:        
+        sess.run(init_op)
+        for epoch in range(epochs):
+            avg_cost = 0
+            batch_x_train = np.array(X_train).reshape(batches, 400)
+            batch_y_train = np.array(y_train).reshape(batches, 1)
+            for i in range(batches):
+                _, c = sess.run(
+                    [optimiser, cross_entropy], feed_dict={
+                        x: batch_x_train, y: batch_y_train})
+                avg_cost += c / batches
+            print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(
+                avg_cost))
+        batch_x_test = np.array(X_test).reshape(batches, 400)
+        batch_y_test = np.array(y_test).reshape(batches, 1)
+        print(sess.run(
+            accuracy, feed_dict={x: batch_x_test, y: batch_y_test}))
+    return sess
 
 
 def mirror_arr(arr_data):
@@ -34,17 +101,24 @@ def prep_data_to_disp(data):
 
 if __name__ == "__main__":
 
-    input_layer_size = 400  # 20x20 Input Images of Digits
-    hidden_layer_size = 25   # 25 hidden units
-    num_labels = 10          # 10 labels, from 1 to 10
+    # size_of_batch = 400  # 20x20 Input Images of Digits
     mat = scipy.io.loadmat('images_data.mat')
 
     X = mat['X']  # 5000x400 (5000 pictures of size 20x20)
     y = mat['y']
 
+    # split test data into training & test set
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.4, random_state=0)
+
     mixed_indices = np.random.permutation(len(X))
 
-    for i in mixed_indices:
-        predicted = 0  # TODO
-        correct = y[i]
-        display_image(prep_data_to_disp(X[i]), img_title(correct, predicted))
+    number_of_batches = 3000  # 60% of total (5000)
+    model = learn_nn_network(
+        X_train, X_test, y_train, y_test, number_of_batches)
+
+    # model verification against picture (demo purposes TODO)
+    # for i in mixed_indices:
+    #     predicted = 0  # TODO
+    #     correct = y[i]
+    #     display_image(prep_data_to_disp(X[i]), img_title(correct, predicted))
